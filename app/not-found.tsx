e1d9be5543afdd479436112ad684cd555ae1976e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-04 21:48:13 -0600

    custom 404 test

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 export default function NotFound() {
-  return <div>
+  return <div className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
       <h1>Not found | 404!</h1>
       <div>
       </div>
