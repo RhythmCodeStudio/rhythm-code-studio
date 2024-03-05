3e75d175d769510b816658d1a8779f685d93e0d3
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-04 21:52:26 -0600

    format 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,17 @@
-export default function NotFound() {
-  return <main className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function FourZeroFour() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, [router]);
+
+  return (
+    <main className="text-black flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
       <h1>Not found | 404!</h1>
-      <div>
-      </div>
-  </main>
+      <div></div>
+    </main>
+  );
 }
