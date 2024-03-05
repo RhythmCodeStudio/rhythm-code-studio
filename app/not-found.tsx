7385a350447268e78218b594209db5f5109ab27b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-04 21:49:53 -0600

    custom 404 retest

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
-  return <div className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
+  return <main className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
       <h1>Not found | 404!</h1>
       <div>
       </div>
-  </div>
+  </main>
 }
