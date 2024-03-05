86e1f1d3da8425fb56f24b284a1422523046c49d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-04 21:31:21 -0600

    edit 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,10 +10,10 @@ export default function FourZeroFour() {
   }, [router]);
 
   return (
-    <div className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
+    <main className="text-black flex-col items-center justify-center bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
       404 - page not found
       Redirecting...
-    </div>
+    </main>
   )
 }
 
