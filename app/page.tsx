53c81738c38b88e8fb53c34e298e80e6d23162d0
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-05-23 13:37:22 -0500

    y spacing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { FaCalendarDays } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center space-y-16">
+    <section className="flex flex-col items-center space-y-12">
       <div className="hidden">
         <h1 className="text-center">Rhythm Code Studio</h1>
         <h2 className="text-sm md:text-xl font-bold text-center">
