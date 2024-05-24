075b38a470a7b9c8c651b56122d8e4d2e02a205c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-05-24 08:06:50 -0500

    landing pg spacing and sizing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { FaCalendarDays } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-content">
+    <section className="md:-mt-12 lg:-mt-20 xl:-mt-28 flex flex-col items-center justify-content">
       {/* <div className="hidden">
         <h1 className="text-center">Rhythm Code Studio</h1>
         <h2 className="text-sm md:text-xl font-bold text-center">
@@ -20,7 +20,7 @@ export default function Home() {
 
       <div className="flex justify-center items-center">
         <Image
-          className="w-7/12 h-auto max-w-200"
+          className="w-1/2 md:w-7/12 h-auto max-w-200"
           src="/images/logos/mark-only-full.png"
           width={796}
           height={816}
@@ -29,7 +29,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mt-12 lg:mt-12 xl:mt-10 mb-8 p-4 text-md sm:text-lg lg:text-2xl font-bold text-center md:hover:transform hover:scale-110 transition-transform">
+      <div className="mt-12 lg:mt-12 xl:mt-28 mb-8 xl:mb-20 p-4 text-sm sm:text-lg lg:text-2xl 2xl:text-3xl font-bold text-center md:hover:transform hover:scale-110 transition-transform">
         <a
           href="https://calendly.com/kevin-jnxz/30min"
           target="_blank"
@@ -37,7 +37,7 @@ export default function Home() {
           <div className="mb-2 flex justify-center items-center">
             <FaCalendarDays size={36} />
           </div>
-          <h3 className=" text-sm md:text-base lg:text-xl underline">
+          <h3 className="underline">
             Schedule a free consultation.
           </h3>
         </a>
@@ -55,7 +55,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
             aria-label="email Rhythm Code Studio">
-            <h4 className="text-lg md:text-xl underline sm:hover:transform hover:scale-110 transition-transform">
+            <h4 className="text-lg md:text-xl lg:text-2xl 2xl:text-4xl underline sm:hover:transform hover:scale-110 transition-transform">
               kevin@rhythmcodestudio.tech
             </h4>
           </a>
