70c02cae6f6f6af74f69741e2c7b3ef8b4e5976e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-04 19:00:42 -0500

    adjustment

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,25 +2,24 @@
 // import from next
 import Image from "next/image";
 // import components
-import ContactLinks from "./contact-links";
+import SocialMediaLinks from "./contact-links";
 
 export const Footer = () => {
   return (
-    <footer className="text-center text-sm m-4  pb-0">
-      <div className="flex justify-center items-center mb-8 mt-0 md:mb-12">
-        <ContactLinks />
+    <footer className="text-center text-sm m-4">
+      <div className="flex justify-center items-center mb-8 mt-0 md:my-12">
+        <SocialMediaLinks />
       </div>
-      <span className="m-6 p-2">© 2024 Rhythm Code Studio</span>
-      <div
-        className="flex justify-center">
+      <span className="">© 2024 Rhythm Code Studio</span>
+      <div className="flex flex-col items-center justify-center mt-2">
         <Image
-         src="/images/logos/mark-only-light.png"
-         width={24}
-         height={24}
-         alt=""
+          src="/images/logos/mark-only-light.png"
+          width={796}
+          height={816}
+          alt=""
+          className="h-auto max-w-8"
         />
       </div>
     </footer>
   );
-}
-  
+};
--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,23 @@
 import Image from "next/image";
 // import from vercel
 import { track } from "@vercel/analytics";
+// import components
+import SocialMediaLinks from "./components/contact-links";
 // import icons
 import { FaCalendarDays } from "react-icons/fa6";
+import { FaEnvelope } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <section className="overflow-hidden flex flex-col items-center justify-content space-y-12 3xl:space-y-36">
+    <section className=" flex flex-col items-center justify-content space-y-16 3xl:space-y-36">
       <div className="hidden">
         <h1 className="text-center">Rhythm Code Studio</h1>
         <h2 className="text-center">Custom web solutions to match your beat</h2>
       </div>
+
       <div className="flex flex-col justify-center items-center">
         <Image
-          className="w-1/2 h-auto max-w-144"
+          className="w-2/5 h-auto max-w-144"
           src="/images/logos/mark-only-full.png"
           width={796}
           height={816}
@@ -24,16 +28,11 @@ export default function Home() {
         />
       </div>
 
-      <div 
-        className="text-md lg:text-lg 3xl:text-xl font-bold  md:hover:transform hover:scale-110 transition-transform">
+      <div className="text-md lg:text-lg 3xl:text-xl font-bold  md:hover:transform hover:scale-110 transition-transform">
         <a
           href="https://calendly.com/kevin-jnxz/30min"
           target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => {
-            track(`Calendly link clicked`);
-          }} 
-        >
+          rel="noopener noreferrer">
           <div className="mb-2 flex justify-center items-center">
             <FaCalendarDays size={36} />
           </div>
