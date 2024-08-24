83ba1306811311eab2955968398706d9b5158366
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-24 15:09:25 -0500

    working on portfolio layout

--- app/lib/project-data.ts
+++ app/lib/project-data.ts
@@ -2,17 +2,25 @@ export const projects = [
   {
     client: "Mike Martin Media",
     project: "Website",
-    description: "A website for a media company.",
+    description: "A website for a media company. A website for a rock band. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl.",
+    features: [
+      "Video Gallery",
+      "Photo Gallery",
+      "Blog",
+      "Contact Form",
+
+      
+    ],
     desktop_image: "/images/projects/mmm/mmm-desktop.png",
     mobile_image: "/images/projects/mmm/mmm-mobile.jpg",
-    url: "https://mikemartinmedia.com",
+    url: "mikemartinmedia.com",
   },
   {
     client: "The Long Emergency",
     project: "Website",
-    description: "A website for a rock band.",
+    description: "A website for a rock band. A website for a media company. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nisl nec nisl.",
     desktop_image: "/images/projects/the-long-emergency/the-long-emergency-desktop.png",
-    mobile_image: "/images/projectsthe-long-emergency-mobile.png",
-    url: "https://thelongemergency.net",
+    mobile_image: "/images/projects/the-long-emergency/the-long-emergency-mobile.jpg",
+    url: "thelongemergency.net",
   },
 ]
--- components/project-gallery.tsx
+++ components/project-gallery.tsx
@@ -8,36 +8,53 @@ import Image from "next/image";
 export default function ProjectGallery() {
   return (
     <>
-      <h2 className="tracking-tight text-center xs:text-lg md:text-2xl lg:text-3xl 3xl:text-5xl ">
+      <h2 className="tracking-tight text-center xs:text-lg sm:text-xl md:text-2xl lg:text-3xl 3xl:text-5xl ">
         CUSTOM WEB SOLUTIONS TO MATCH YOUR BEAT
       </h2>
-      <ul className="grid grid-cols-1 md:grid-cols-2">
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-16">
         {projects.map((project: Project) => (
-          <li key={project.client}>
-            <h3>{project.client}</h3>
-            <div className="flex items-center justify-center p-6 ">
-              <div className="w-52 md:w-72  h-auto p-6">
-            <Image 
-              src={project.mobile_image} 
-              alt={project.client} 
-              width={1080}
-              height={1920}
-            />
+          <li key={project.client} className="shadow-2xl m-6">
+            <h3 className="text-center font-bold text-lg sm:text-xl md:text-2xl mt-4">
+              {project.client}
+            </h3>
+            <h4 className="text-center font-bold sm:text-lg md:text-xl mb-2">
+              {project.url}
+            </h4>
+            
+            <div className="flex items-center justify-center">
+              <div className="w-52 md:w-72 h-auto">
+                <figure className="">
+                  <Image
+                    src={project.mobile_image}
+                    alt={project.client}
+                    width={1080}
+                    height={1920}
+                  />
+                  <figcaption className="text-center font-bold">
+                    Mobile View
+                  </figcaption>
+                </figure>
+              </div>
             </div>
+            <div className="p-2">
+              <p className="px-4">{project.description}</p>
             </div>
-            <h4>{project.project}</h4>
-            <p>{project.description}</p>
+            <div className="flex items-center justify-center">
             <div className="w-96 md:w-128 lg:w-200 h-auto p-6">
-            <Image 
-              src={project.desktop_image} 
-              alt={project.client} 
-              width={1920}
-              height={1080}
-            />
+              <figure>
+                <Image
+                  src={project.desktop_image}
+                  alt={project.client}
+                  width={1920}
+                  height={1080}
+                />
+                <figcaption className="text-center font-bold">
+                  Desktop View
+                </figcaption>
+              </figure>
             </div>
-            <a href={project.url} target="_blank" rel="noopener noreferrer">
-              Visit {project.client}
-            </a>
+            </div>
+            
           </li>
         ))}
       </ul>
