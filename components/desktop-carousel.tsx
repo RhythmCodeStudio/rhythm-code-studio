56fae752c365404ef1652d0f8d62001a42663a56
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-26 13:16:13 -0500

    update portfolio, desktop carousel, added features

--- components/desktop-carousel.tsx
+++ components/desktop-carousel.tsx
@@ -12,7 +12,7 @@ export default function DesktopCarousel(props: PropType) {
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
   return (
-    <div className="bg-zinc-900  embla__viewport rounded-2xl border-4 border-zinc-900" ref={emblaRef}>
+    <div className="embla__viewport rounded-2xl border-4 border-zinc-900 bg-zinc-800" ref={emblaRef}>
       <div className="embla__container">
         {slides.map((slide, index) => (
           <div className="embla__slide" key={index}>
@@ -22,7 +22,6 @@ export default function DesktopCarousel(props: PropType) {
               alt={`Slide ${index + 1}`}
               width={1920}
               height={1080}
-              className=""
             />
           </div>
         ))}
--- components/mobile-carousel.tsx
+++ components/mobile-carousel.tsx
@@ -12,7 +12,7 @@ export default function MobileCarousel(props: PropType) {
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
   return (
-    <div className="embla__viewport" ref={emblaRef}>
+    <div className="embla__viewport rounded-2xl border-4 border-zinc-900 bg-zinc-800" ref={emblaRef}>
       <div className="embla__container">
         {slides.map((slide, index) => (
           <div className="embla__slide" key={index}>
@@ -22,7 +22,7 @@ export default function MobileCarousel(props: PropType) {
               alt={`Slide ${index + 1}`}
               width={1080}
               height={1920}
-              className="rounded-2xl border-4 border-zinc-900"
+              className=""
             />
           </div>
         ))}
