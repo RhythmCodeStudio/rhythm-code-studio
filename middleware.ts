5ac52cb976a1ef9d852b1c97dc02e4ac47ab672c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-26 10:56:55 -0500

    update csp for email js

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
+    connect-src 'self' https://api.emailjs.com/api/v1.0/email/send;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
