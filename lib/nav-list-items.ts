077584afcb4943933a93a7a88aa2f6375f4679ac
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-13 21:05:38 -0500

    update

---
+++ lib/nav-list-items.ts
@@ -0,0 +1,17 @@
+export const navListItems = [
+  {
+    label: "About",
+    href: "/about",
+    htmlElement: "internalLink",
+  },
+  {
+    label: "Portfolio",
+    href: "/portfolio",
+    htmlElement: "internalLink",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+    htmlElement: "internalLink",
+  },
+];
