412e3ef95c30e0606b8c0f2e3eb03c21b5c71d4c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-14 17:44:18 -0500

    update not found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,8 @@
 "use client";
 // import from next
 import { connection } from "next/server";
-
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function NotFound() {
   await connection();
@@ -9,7 +10,17 @@ export default async function NotFound() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-2xl font-bold">Page Not Found</h1>
-      <p className="mt-4">Redirecting to home...</p>
+      <p className="mt-4">The page you are looking for does not exist.</p>
+      <Link href="/"> 
+        <Image
+          className="w-full h-auto max-w-40 xl:max-w-48 3xl:max-w-64 3xl:m-4"
+          src="/images/logos/horizontal-full-no-tag.png"
+          width={1000}
+          height={54}
+          alt="Rhythm Code Studio - Custom web solutions to match your beat"
+          priority
+        />
+        </Link>
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
         <Link href="/"> 
         <Image
           className="w-full h-auto max-w-40 xl:max-w-48 3xl:max-w-64 3xl:m-4"
-          src="/images/logos/horizontal-full-no-tag.png"
+          src="/images/logos/horizontal-full.png"
           width={1000}
           height={54}
           alt="Rhythm Code Studio - Custom web solutions to match your beat"
