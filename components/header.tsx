43fc3ca2f7a9d5eecdaca61ae2d557b7ef05969d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-14 13:07:37 -0500

    update

--- components/header.tsx
+++ components/header.tsx
@@ -19,8 +19,18 @@ export default function Header() {
   const isHome = pathname === "/";
   
   return (
-    <header className="m-4">
+    <header className="m-4 p-4">
       <div className="flex justify-between items-center">
+        {isHome ? (
+          <Image
+          className="w-full h-auto max-w-40 xl:max-w-48 3xl:max-w-64 3xl:m-4"
+          src="/images/logos/horizontal-full-no-tag.png"
+          width={1000}
+          height={54}
+          alt="Rhythm Code Studio - Custom web solutions to match your beat"
+          priority
+        />
+        ) : (
         <Link href="/"> 
         <Image
           className="w-full h-auto max-w-40 xl:max-w-48 3xl:max-w-64 3xl:m-4"
@@ -31,6 +41,7 @@ export default function Header() {
           priority
         />
         </Link>
+        )}
         <div className="3xl:mr-12">
         <Nav />
         </div>
