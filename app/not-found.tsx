04d70415433c08ae99ff8172a9459bcb21f66a86
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-14 17:50:59 -0500

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,19 +8,21 @@ export default async function NotFound() {
   await connection();
 
   return (
-    <div className="p-8 text-center">
+    <div className="p-8 text-center flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Page Not Found</h1>
       <p className="mt-4">The page you are looking for does not exist.</p>
       <Link href="/"> 
         <Image
           className="w-full h-auto max-w-40 xl:max-w-48 3xl:max-w-64 3xl:m-4"
-          src="/images/logos/horizontal-full-no-tag.png"
+          src="/images/logos/mark-only-full.png"
           width={1000}
           height={54}
           alt="Rhythm Code Studio - Custom web solutions to match your beat"
           priority
         />
         </Link>
+        <h2 className="font-blenny text-2xl">Rhythm Code Studio</h2>
+        <h3 className="text-lg">Custom web solutions to match your beat</h3>
     </div>
   );
 }
