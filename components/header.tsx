396592eeebc118e9b30f9a95b2f95b671e6d20ad
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-09-19 09:12:57 -0500

    update header logo

--- components/header.tsx
+++ components/header.tsx
@@ -20,32 +20,31 @@ export default function Header() {
   return (
     <header className="m-4 mb-12 lg:mb-6 lg:m-6 p-2 lg:p-6 ">
       <div className="flex justify-between items-center">
-        {isHome ? (
-          <Image
-            className="w-full h-auto max-w-40 md:max-w-48 lg:max-w-50 2xl:max-w-64"
-            src="/images/logos/horizontal-full-no-tag-256x138.png"
-            width={256}
-            height={138}
-            alt="Rhythm Code Studio - Custom web solutions to match your beat"
-            priority
-            sizes="(max-width: 768px) 160px, (max-width: 1280px) 192px, (max-width: 1536px) 200px, 256px"
-          />
-        ) : (
-          <Link href="/">
+        <div className="w-full max-w-40 md:max-w-48 lg:max-w-50 2xl:max-w-64">
+          {isHome ? (
             <Image
-              className="w-full h-auto max-w-40 md:max-w-48 lg:max-w-50 2xl:max-w-64"
+              className="w-full h-auto"
               src="/images/logos/horizontal-full-no-tag-256x138.png"
               width={256}
               height={138}
               alt="Rhythm Code Studio - Custom web solutions to match your beat"
-              priority={true}
+              priority
               sizes="(max-width: 768px) 160px, (max-width: 1280px) 192px, (max-width: 1536px) 200px, 256px"
             />
-          </Link>
-        )}
-        {/* <div className="3xl:mr-12">
-          {/* <Nav /> *
-        </div> */}
+          ) : (
+            <Link href="/" className="block">
+              <Image
+                className="w-full h-auto"
+                src="/images/logos/horizontal-full-no-tag-256x138.png"
+                width={256}
+                height={138}
+                alt="Rhythm Code Studio - Custom web solutions to match your beat"
+                priority
+                sizes="(max-width: 768px) 160px, (max-width: 1280px) 192px, (max-width: 1536px) 200px, 256px"
+              />
+            </Link>
+          )}
+        </div>
         <div className="hidden lg:flex items-center">
           <DesktopNav navListItems={navListItems} />
         </div>
