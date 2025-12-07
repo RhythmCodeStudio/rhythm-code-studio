1bcac89637b2ccf634a2515a9c896bc15765e2b2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-07 10:37:07 -0600

    revert commit and fix menu icon links

--- components/contact-icon-links.tsx
+++ components/contact-icon-links.tsx
@@ -1,8 +1,11 @@
 "use client";
 // import from next
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 // import link data
 import { contactIconLinkData } from "../lib/contact-icon-link-data";
+// import themes
+import { useTheme } from "../context/theme-context";
+import { themes } from "../lib/themes";
 // import components
 import IconLink from "./icon-link";
 
@@ -24,13 +27,18 @@ export default function ContactIconLinks({
   include,
   size,
 }: ContactIconLinkProps) {
-  const pathname = usePathname();
-  const isHome = pathname === "/";
+  // const pathname = usePathname();
+  // const isHome = pathname === "/";
 
-  // Don't render on the home page
-  if (isHome) {
-    return null;
-  }
+  // if (isHome) {
+  //   return null;
+  // }
+
+  const { theme } = useTheme();
+  const themeObj = themes[theme];
+  const logoInvert = themeObj.logoInvert;
+  const color = themeObj.color;
+  const textShadow = themeObj.textShadow;
 
   // Filter links if include is provided
   const filteredLinks = include
@@ -44,12 +52,12 @@ export default function ContactIconLinks({
 
   return (
     <div
-      className={`flex items-center space-x-12 ${
+      className={`flex items-center space-x-12 text-${color} ${
         orientation === "vertical" ? "flex-col" : "flex-row"
       }`}
     >
       {filteredLinks.map(({ href, icon: Icon, label }: ContactIconLinkData) => (
-        <IconLink key={href} href={href} icon={Icon} size={iconSize} label={label} />
+        <IconLink key={href} href={href} icon={Icon} size={iconSize} label={label}  />
       ))}
     </div>
   );
